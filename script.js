17d8b00fe068b07ca26d430b9d3434c651bfbd32
its-namami <herrmannjohann@tuta.io>
2024-04-04 16:34:32 +0200

    Add .

---
+++ script.js
@@ -0,0 +1,168 @@
+
+let buttons = document.querySelectorAll(".one-button");
+let areals = document.querySelectorAll(".areal");
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', function () {
+        buttons.forEach(btn => btn.classList.remove('active'));
+    this.classList.add('active'); 
+
+      // function to activate relative content
+    equalizer(index);
+  });
+});
+
+
+function equalizer(activeButtonIndex){
+    areals.forEach((item, itemIndex) => {
+        if (itemIndex === activeButtonIndex) {
+            areals.forEach(item => item.classList.remove('active'));
+            item.classList.add('active');
+        }
+    })
+};
+
+let visible = document.querySelectorAll(".matryoshka");
+let downs = document.querySelectorAll(".hidden-reich");
+
+
+visible.forEach((clicked, index) => {
+    clicked.addEventListener('click', function (e) {
+        if (this.classList.contains("on")) {
+            this.classList.remove('on'); 
+        } else {
+            visible.forEach(one => one.classList.remove('on'));
+            this.classList.add('on'); 
+        }
+  });
+});
+
+
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* html deleted tiles
+<div class="tiles">
+        <div class="ONE-clickables">
+            <button id="tiles-z">ONE Team</button>
+            <button id="tiles-z">ONE Karriere</button><!-- the default one-->
+            <button id="tiles-z">ONE Arbeitgeber</button>
+        </div>
+        <div class="ONE-content">
+            <img id="ONE-image">
+            <div class="ONE-text-placeholder">
+                <p></p>
+            </div>
+
+        </div>
+    </div>
+
+
+CSS deleted styles
+
+.tiles{
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    border: 1px solid blue;
+}
+
+.ONE-content{
+    display: flex;
+    flex-wrap: wrap;
+    border: 1px solid blue;
+}
+
+.ONE-content img{
+    width: 50%;
+    margin-top: 50px;
+}
+
+.ONE-image-placeholder{
+    width: 50%;
+}
+
+.ONE-text-placeholder{
+    display: flex;
+    align-items: center;
+    width: 50%;
+}
+
+.ONE-text-placeholder p{
+    padding: 0 20% 0 10%;
+    line-height: 24px;
+}
+
+button{
+    cursor: pointer;
+}
+
+
+
+ */
+
+/* the underline, when active
+.ONE-clickables button:focus{
+    color: black;
+    font-weight: 900;
+}
+
+.ONE-clickables button:after{
+    content: "";
+    display: block;
+    width: 100%;
+    height: 3px;  Adjust the height of the line as needed 
+    background-color: red;  Change the color of the line 
+    margin-top: 5px; /* Adjust the distance between the button and the line 
+}*/
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+      nextEl: '.custom-next',
+      prevEl: '.custom-prev',
+    },
+  });
+  
